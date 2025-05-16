b25d97c0e51430b3edcf593475fb5d562bb79b97
sheriff <chege2452@gmail.com>
2025-05-16 12:05:08 +0300

    new resume

--- src/data/personalInfo.ts
+++ src/data/personalInfo.ts
@@ -6,7 +6,7 @@ export const personalInfo = {
   description: 'Passionate software engineer with 7+ years of experience specializing in full-stack development. I build scalable, user-friendly applications with modern technologies and best practices.',
   location: 'Nairobi, Kenya',
   email: 'machariachege20@gmail.com',
-  resumeUrl: '/resume.pdf'
+  resumeUrl: 'https://docs.google.com/document/d/1_ug954IFyOMvidKcQPvKhSNZPHjS5L_o69Tu5qCGi3Q/edit?usp=sharing'
 };
 
 export const socialLinks: SocialLink[] = [
